406eeef421145893ef5a7a9365132b7fe32f5794
林佳蓉 <s6122712@gmail.com>
2017-08-13 12:24:45 +0800

    add social media
    add social media

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
+/*Twitter==================================================================*/
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+
 /*--- 變數宣告 ---*/
 
 var mainNavTotal=$('.topSide nav li').length;		//主選單的數目 
